c6305c58a5ff419ae4c714a5704ddee9bbc55225
Matthias MARTIN <mmartin@e1r4p9.42.fr>
2015-04-17 12:17:04 +0200

    pow, abs

--- math/ft_abs.c
+++ math/ft_abs.c
@@ -1,33 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_pow.c                                           :+:      :+:    :+:   */
+/*   ft_abs.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmartin <mmartin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/02/01 19:07:37 by mmartin           #+#    #+#             */
-/*   Updated: 2015/04/05 12:36:25 by mmartin          ###   ########.fr       */
+/*   Created: 2015/04/05 12:18:21 by mmartin           #+#    #+#             */
+/*   Updated: 2015/04/05 12:19:03 by mmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-double		ft_pow(double base, int exp)
+double	ft_abs(double n)
 {
-	int		flag;
-	double	result;
-
-	flag = 0;
-	result = 1;
-	if (exp < 0)
-	{
-		exp *= -1;
-		flag = 1;
-	}
-	while (exp)
-	{
-		if (exp & 1)
-			result *= base;
-		exp >>= 1;
-		base *= base;
-	}
-	return ((flag ? 1 / result : result));
+	return ((n < 0 ? -n : n));
 }
