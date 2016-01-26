b4b1ace795b8df7e7f7c1448301fffc3e4a3d442
Matthias MARTIN <mmartin@e1r4p9.42.fr>
2016-01-26 13:55:55 +0100

    norme

--- includes/ft_math.h
+++ includes/ft_math.h
@@ -6,7 +6,7 @@
 /*   By: mmartin <mmartin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/25 19:40:17 by mmartin           #+#    #+#             */
-/*   Updated: 2016/01/25 19:55:44 by mmartin          ###   ########.fr       */
+/*   Updated: 2016/01/26 13:55:48 by mmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,21 +47,20 @@ typedef struct	s_mat3
 **	ft_vec3.c
 */
 
-t_vec3		ft_cross_vec3(t_vec3 a, t_vec3 b);
-float		ft_dot_vec3(t_vec3 a, t_vec3 b);
-t_vec3		ft_sub_vec3(t_vec3 a, t_vec3 b);
-t_vec3		ft_add_vec3(t_vec3 a, t_vec3 b);
-t_vec3		ft_mul_vec3(t_vec3 a, float b);
+t_vec3			ft_cross_vec3(t_vec3 a, t_vec3 b);
+float			ft_dot_vec3(t_vec3 a, t_vec3 b);
+t_vec3			ft_sub_vec3(t_vec3 a, t_vec3 b);
+t_vec3			ft_add_vec3(t_vec3 a, t_vec3 b);
+t_vec3			ft_mul_vec3(t_vec3 a, float b);
 
 /*
 **	ft_vec4.c
 */
 
-t_vec4		ft_cross_vec4(t_vec4 a, t_vec4 b);
-float		ft_dot_vec4(t_vec4 a, t_vec4 b);
-t_vec4		ft_sub_vec4(t_vec4 a, t_vec4 b);
-t_vec4		ft_add_vec4(t_vec4 a, t_vec4 b);
-t_vec4		ft_mul_vec4(t_vec4 a, float b);
-
+t_vec4			ft_cross_vec4(t_vec4 a, t_vec4 b);
+float			ft_dot_vec4(t_vec4 a, t_vec4 b);
+t_vec4			ft_sub_vec4(t_vec4 a, t_vec4 b);
+t_vec4			ft_add_vec4(t_vec4 a, t_vec4 b);
+t_vec4			ft_mul_vec4(t_vec4 a, float b);
 
 #endif
